08c106db6c563a5a64b04a9da1408612fd39bd0e
sema.ozturk <sema.ozturk@okyanusteknoloji.com.tr>
2020-08-20 17:32:07 +0300

    Add project files.

---
+++ RedisTutorial/Controllers/CacheController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RedisTutorial.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RedisTutorial.Controllers
+{
+    [Route("api/[controller]")]
+    public class CacheController : Controller
+    {
+        private readonly ICacheService _cacheService;
+
+        public CacheController(ICacheService cacheService)
+        {
+            _cacheService = cacheService;
+        }
+        [HttpGet("cache/{key}")]
+        public async Task<IActionResult> GetCacheValue([FromRoute] string key)
+        {
+            var value = await _cacheService.GetCacheValueAsync(key);
+            return string.IsNullOrEmpty(value) ? (IActionResult) NotFound() : Ok(value);
+        }
+        [HttpPost("cache")]
+
+        public async Task<IActionResult> SetCacheValue([FromBody] NewCacheEntryRequest request)
+        {
+            await _cacheService.SetCacheValueAsync(request.Key, request.Value);
+            return Ok();
+        }
+    }
+}
---
+++ RedisTutorial/Services/RedisCacheService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace RedisTutorial.Services
+{
+    public class RedisCacheService : ICacheService
+    {
+        private readonly IConnectionMultiplexer _connection;
+
+        public RedisCacheService(IConnectionMultiplexer connection)
+        {
+            _connection = connection;
+        }
+
+        public async Task<string> GetCacheValueAsync(string key)
+        {
+            var db = _connection.GetDatabase();
+           return await  db.StringGetAsync(key);//from a json string to a poco object and returns that poco object
+        }
+
+        public async Task SetCacheValueAsync(string key, string value)
+        {
+            var db = _connection.GetDatabase();
+            await db.StringSetAsync(key, value);
+        }
+    }
+}
